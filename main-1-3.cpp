6da4d90ef6bde55bc124485584aaac7d2ea2a192
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:36:02 +0930

    Update main-1-3.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-int *ptr, i=42, k=477, l=43;
-
+void count_digits(int array[4][4]), i=42, k=477, l=43;
+/* address of i is assigned to ptr */
 ptr = &i;   
-cout<< "Value of i " << *ptr << endl; 
-cout<< "Value of k " << *(ptr+1) << endl;
-cout<< "Value of l " << *(ptr+2) << endl;
-cout<< "ptr " << ptr << endl;
-cout<< "ptr +1  " << (ptr+1) << endl;
+cout<< "Value of i " << *void count_digits(int array[4][4]) << endl; 
+cout<< "Value of k " << *(void count_digits(int array[4][4])+1) << endl;
+cout<< "Value of l " << *(void count_digits(int array[4][4])+2) << endl;
+cout<< "void count_digits(int array[4][4]) " << void count_digits(int array[4][4]) << endl;
+cout<< "void count_digits(int array[4][4]) +1  " << (void count_digits(int array[4][4])+1) << endl;
 
 return 0;
 }
