bef3ced2da6e00a3e97ce865224574f92fac68b8
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:29:09 +0930

    Update function-1-3.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -3,14 +3,12 @@
 void count_digits(int array[4][4])
 
 {
-
-int k=0,l=0;
   
 int *ptr, i=42, k=477, l=43;
 
 //count array to store values
 
-int count_array[4]={0};
+int count_array[10]={0};
 
 //loop to count occurances
 
