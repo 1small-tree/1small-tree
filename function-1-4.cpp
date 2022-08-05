710e13d78ec32dbddee93e0d94dec2611c792a4f
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:03:28 +0930

    Update function-1-4.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,47 +1,21 @@
-void print_scaled_matrix(int array[3][3], int scale)
+#include <iostream>
 
+using namespace std;
+void print_scaled_matrix(int array[3][3],int scale)
 {
-
-for(int i=0; i<3; i++)
-
-{
-
-for(int j=0; j<3; j++)
-
-{
-
-cout << array[i][j]*scale << " ";
-
+    for(int i=0;i<3;i++) { //loop to rows 
+        for(int j=0;j<3;j++){ //loop to columns 
+        
+            cout<<array[i][j]*scale<<" ";
+        }
+        cout<<endl; //print new line 
+    }
 }
-
-cout << endl;
-
-}
-
-}
-
-void print_summed_matrices(int array1[3][3],int array2[3][3])
-
-{
-
-for(int i=0; i<3; i++)
-
-{
-
-for(int j=0; j<3; j++)
-
-{
-
-cout << array1[i][j]+array2[i][j] << " ";
-
-}
-
-cout << endl;
-
-}
-
-}
-
 int main()
-
 {
+    int scale=3;
+    int threebythree[3][3]={{0,1,2},{3,4,5},{6,7,8}};
+    print_scaled_matrix(threebythree,scale); //call 
+
+    return 0;
+}
