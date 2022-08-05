a903fdc12b3fccb3a2259a1e7397549ac2422ac9
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 21:27:31 +0930

    Update function-1-1.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,17 +1,8 @@
-int diagonal(int array[4][4])
-
-{
-
-int diagTotal = 0;
-
-for (int i = 0; i < 4; ++i)
-
-{
-
-diagTotal = diagTotal + array[i][i];
-
-}
-
-return diagTotal;
-
+//to function
+int sum_diagonal(int array[4][4]){
+    int sum=0;
+    for(int i=0;i<4;i++){
+        sum += array[i][i];
+    }
+    return sum;
 }
