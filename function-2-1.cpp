2f7c158000a521f4f060d3a9d62c1411d7cf89c2
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 23:06:43 +0930

    Update function-2-1.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void print as binary(std::string desimal number){
+void print_binary_str(std::string decimal_number){
 if(decimal number.length()>=1 && decimal numberlength()<=9){
 int formatted dec=stoi(str:decimalnumber);
 string binary =bitset<10>(val:formatted dec).to string(); cout<< stoi( str: binary)<<endl;
