a5e0ece66246a1aa04e9bc999b41764b9e2d0f5f
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:52:26 +0930

    Update function-1-4.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,22 +1,65 @@
 #include <iostream>
 
 using namespace std;
-void print_scaled(int array[3][3],int scale)
+
+class matrix
+
 {
-    
-    for(int i=0;i<3;i++) { 
-        for(int j=0;j<3;j++){  
-        
-            cout<<array[i][j]*scale<<" ";
-        }
-        cout<<endl; 
-    }
+
+
+void print_scaled_matrix(int arr[3][3],int scale)
+
+{
+
+int i,j;
+
+for(i=0;i<3;i++)
+
+{
+
+for(j=0;j<3;j++)
+
+{
+
+arr[i][j]=arr[i][j]*3;
+
 }
-int main()
+
+}
+
+cout<<"The resultant matrix is"<<endl;
+
+for(i=0;i<3;i++)
+
 {
-    int scale = 3 ;
-int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}} ;
-print_scaled(threebythree,scale) ;
-     
-    return 0;
+
+for(j=0;j<3;j++)
+
+{
+
+cout<<arr[i][j]<<" ";
+
+}
+
+cout<<"\n";
+
+}
+
+}
+
+};
+
+int main()
+
+{
+
+matrix obj;
+int scale=3; 
+
+int threebythree[3][3]={{0,1,2},{3,4,5},{6,7,8}};
+
+obj.print_scaled_matrix(threebythree,scale);
+
+return 0;
+
 }
