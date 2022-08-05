2d330298d6770bb1089247b2812635ec6773ab47
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 23:48:42 +0930

    Update main-2-1.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-int *ptr;
+int *void print_binary_str(std::string decimal_number);
 int a[3] = {43,477,34};
 /* address of a[0] is assigned to ptr */
 ptr = &a[0];   
