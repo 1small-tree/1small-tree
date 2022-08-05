4524ef886a976b3c945e4910a42088fec21618bf
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 21:34:10 +0930

    Update function-1-5.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void print_summed_matrices(int array1[3][3],int array2[3][3])
-{
-    for(int i=0;i<3;i++) //loop to rows 
-    {
-        for(int j=0;j<3;j++) //loop to column
-        {
-            cout<<array1[i][j]+array2[i][j]<<" "; //add together 
-        }
-        cout<<endl; //newline 
-    }
-    
-    
+void print scaled matrix(int array[3][3], int scale) {
+for (int i=0; i<3;i++) {
+for (int j=0;j<3;j++) {
+array[i][j] = scale * array[i][j]; 
+  cout << array[i][j]<" ";
 }
-int main()
-{
-    
-    int matrix1[3][3]={{0,1,2},{3,4,5},{6,7,8}};
-    int matrix2[3][3]={{0,0,0},{2,2,2},{-5,-4,8}};
-    print_summed_matrices(matrix1,matrix2); //call 
-
-    return 0;
+cout << endl;
 }
