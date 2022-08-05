b86f2284d93c313a3c760875964135e819db3abd
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 23:49:36 +0930

    Update function-1-5.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
- void print_summed(int array1[3][3],int array2[3][3])
-{
-    for(int i=0;i<3;i++) //loop to rows 
-    {
-        for(int j=0;j<3;j++) //loop to column
-        {
-            cout<<array1[i][j]+array2[i][j]<<" "; //add together 
-        }
-        cout<<endl; //newline 
-    }
-    
-    
-}
 int main()
 {
-    
-    int matrix1[3][3]={{0,1,2},{3,4,5},{6,7,8}};
-    int matrix2[3][3]={{0,0,0},{2,2,2},{-5,-4,8}};
-    print_summed_matrices(matrix1,matrix2); //call 
-
-    return 0;
+int *ptr,i,k ;
+int *ptr2,L ;
+/* address of i is assigned to ptr */
+ptr = &i;
+/* ptr2 is assigned address of next int after i */
+ptr2 = ptr + 1 ;
+/* L is the number of ints between ptr and ptr2 */
+L = ptr2 - ptr ;
+cout << "Value of i    " << *ptr << endl;
+cout << "Value of ptr? " << *(ptr+1)<< endl;
+cout << "Value of ptr  " <<  (unsigned long) ptr << endl;
+cout << "Value of ptr2 " << (unsigned long) ptr2 << endl;
+cout << "Value of L    " << L << endl;
+return 0;
 }
