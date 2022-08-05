bab187066dd834d2b70b78b31ff24539cf296920
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 20:51:14 +0930

    Create main-2-1.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

---
+++ .github/main-2-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int *ptr;
+int a[3] = {43,477,34};
+/* address of a[0] is assigned to ptr */
+ptr = &a[0];   
+cout << "Value of a[0] : "<< *(ptr+0) << endl;
+cout << "Value of a[1] : "<< *(ptr+1) << endl;
+cout << "Value of a[2] : "<< *(ptr+2) << endl;
+return 0;
+}
