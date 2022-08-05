b7ec24c5612022f9f17096a446718b4dcf14cfb6
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:43:00 +0930

    Update function-1-4.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -16,8 +16,8 @@ int main()
 {
     int scale=3;
     int threebythree[3][3]={{0,1,2},{3,4,5},{6,7,8}};
-    
-     print_scaled_matrix(threebythree,scale) ; //call 
      print_scaled 
+     print_scaled_matrix(threebythree,scale) ; //call 
+     
     return 0;
 }
