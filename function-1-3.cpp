18d4ffd6d7f6f0e302deade8dfe7ff591f596596
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:10:29 +0930

    Update function-1-3.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,96 +1,25 @@
-void count_numbers(int array[4][4])
+#include <iostream>
+#include<iomanip>
+#include<fstream>
+#include <stdint.h>
+#include <string>
+#include <cmath>
+using namespace std;
 
-{
-
-int num[10] = {0};
-
-for(int i=0; i<4; i++)
-
-{
-
-for(int j=0; j<4; j++)
-
-{
-
-switch(array[i][j])
-
-{
-
-case 0:
-
-num[0] += 1;
-
-break;
-
-case 1:
-
-num[1] += 1;
-
-break;
-
-case 2:
-
-num[2] += 1;
-
-break;
-
-case 3:
-
-num[3] += 1;
-
-break;
-
-case 4:
-
-num[4] += 1;
-
-break;
-
-case 5:
-
-num[5] += 1;
-
-break;
-
-case 6:
-
-num[6] += 1;
-
-break;
-
-case 7:
-
-num[7] += 1;
-
-break;
-
-case 8:
-
-num[8] += 1;
-
-break;
-
-case 9:
-
-num[9] += 1;
-
-break;
-
+void count_digits(int array[4][4]){
+  
+int flag = 0, i, j;
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+        {
+            if ((array[i][j] != 1) && (array[j][i] != 0))
+            {
+                flag = 1;
+                break;
+            }
+        }
+    if (flag == 0)
+        return 1;
+    else
+        return 0;
 }
-
-}
-
-}
-
-for(int i=0; i<10; i++)
-
-{
-
-cout << i << ":" << num[i] << ";";
-
-}
-
-cout << endl;
-
-}
-
