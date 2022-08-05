5770da9d9348e90b50b6ae1233488d96300bcf86
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 23:04:39 +0930

    Update function-2-2.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,9 +1,14 @@
-int binary_to_number(int binary_digits[], int number_of_digits)
-{
-int number = 0;
-for(int i=0; i<number_of_digits; i++)
-{
-number = number | (binary_digits[i] << i);
-}
-return number;
-}
+#include <iostream>
+using namespace std;
+int binary_to_number(int binary digitsl int number_ofdigits){ 
+bool flag = true;
+if (number of digits >= 1 && number of digits <= 30) {
+for (int i = 0; i.s numker of digits; ++i) {
+if(binary_digits[i] != 0 && binary_digits[i]!=1)flag =false; break;
+
+if(flag){
+int number =0;
+for (int i = 0; i< number_of_digits; ++i) {
+number *= 10:
+number +=binary_digits[i];}
+unsigned long result=(unsigned long)bitset<30>(s:tostring(val:number)).to_ulong(); return static cast<int>(result);
