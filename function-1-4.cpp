550bc3b94992a33c39e23e598e2875167f8f4d99
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:39:46 +0930

    Update function-1-4.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 void print_scaled(int array[3][3],int scale)
 {
+    
     for(int i=0;i<3;i++) { //loop to rows 
         for(int j=0;j<3;j++){ //loop to columns 
         
@@ -15,7 +16,8 @@ int main()
 {
     int scale=3;
     int threebythree[3][3]={{0,1,2},{3,4,5},{6,7,8}};
-    print_scaled_matrix(threebythree,scale); //call 
-
+    
+     print_scaled_matrix(threebythree,scale) ; //call 
+     print_scaled 
     return 0;
 }
