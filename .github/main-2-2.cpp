a0b6bbe99185c7963938bd5d5b869fd821153522
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 20:51:42 +0930

    Create main-2-2.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

---
+++ .github/main-2-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a[10] = {1,2,3,4,5,6,7,8,9,10};
+int *ip;
+ip = &a[2];
+cout << "array address: " << &a[0] << endl;
+//or
+cout << "array address: " << a << endl;
+for (int i = 0 ; i < 10 ; i++ ){
+ cout << "a[" << i << "] = " << a[i] << endl;
+}
+cout << "---------------\n";
+*ip = 500; 
+for (int i = 0 ; i < 10 ; i++ ){
+cout << "a[" << i << "] = " << a[i] << endl;
+}
+return 0;
+}
