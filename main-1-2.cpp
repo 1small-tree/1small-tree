1737b3cce967339512dc2164b55f63964309d18d
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 21:28:26 +0930

    Update main-1-2.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include<iomanip>
+#include<fstream>
+#include <stdint.h>
+#include <string>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-int *ptr, i;
-i = 11;  
-/* address of i is assigned to ptr */
-ptr = &i;   
-/* increment the value of i */
-(*ptr)++;
-/* show i's value using ptr variable */
-cout << "Value of i: "  << *ptr << endl;      
-return 0;
+    int i, j, array[10][10], identityFlag;
+    
+    cout << "Enter array elements : ";
+    for (i = 0; i < 10; i++)
+        for (j = 0; j < 10; j++)
+            cin >> array[i][j];
+
+    identityFlag = array[i][j];
+    
+    if (identityFlag == 1)
+    {
+        cout << "Identity Matrix";
+    }
+    else
+    {
+        cout << "Not an Identity Matrix";
+    }
+    return 0;
 }
