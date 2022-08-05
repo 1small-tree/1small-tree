4c96c6b1f4363d9e7a2a2ab7c8cce1ed6f1793f4
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 23:09:46 +0930

    Update function-2-1.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,8 +1,64 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include <string>
+#include<cstring>
+#include <cstdio>
+#define max 1000000
 using namespace std;
-void print_binary_str(std::string decimal_number){
-if(decimal number.length()>=1 && decimal numberlength()<=9){
-int formatted dec=stoi(str:decimalnumber);
-string binary =bitset<10>(val:formatted dec).to string(); cout<< stoi( str: binary)<<endl;
+
+
+
+void print_binary_str(std::string decimal_number)
+{
+
+// converting string into int number
+
+long long int number=stoi(decimal_number);
+
+cout<<"\ndecimal number is "<<number<<endl;
+
+// declaration
+long long int temp=number;
+int r;
+
+long long int binary_str[max];
+int i=-1;
+
+// finding binary by dividing 2 and on looking remainder
+while(temp!=0)
+{
+// remainder
+r=temp%2;
+// storing in binary_str
+binary_str[++i]=r;
+// next number after dividing by 2
+temp=temp/2;
 }
+
+cout<<"\nCorresponding binary of decimal number: \n";
+int j;
+
+// printing from last
+for(j=i;j>=0;j--)
+cout<<binary_str[j]<<" ";
+cout<<endl;
+}
+
+// main fnction
+
+int main()
+{
+// declaration
+string decimal_number;
+int n;
+
+cout<<"enter number to convert into binary : ";
+cin>>decimal_number;
+
+// function calling
+print_as_binary(decimal_number);
+
+// returning from main function
+
+return 0;
+
 }
