7299d092d653121824987fb9c1565411a92ddd7a
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-05 22:19:49 +0930

    Update function-1-4.cpp
    Signed-off-by: 1small-tree <104819100+1small-tree@users.noreply.github.com>

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-void print_scaled_matrix(int array[3][3],int scale)
+void print_scaled(int array[3][3],int scale)
 {
     for(int i=0;i<3;i++) { //loop to rows 
         for(int j=0;j<3;j++){ //loop to columns 
